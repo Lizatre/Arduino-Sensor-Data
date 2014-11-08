151c5415fc4171bd6cc9a24056d556af9b178b31
Sirblobfish <s.utkarsh@berkeley.edu>
2014-11-08 15:22:28 -0800

    Add more comments and compress the average code into one line

--- Arduino-sensor-data.ino
+++ Arduino-sensor-data.ino
@@ -1,13 +1,10 @@
 /*
 This program reads sensor values (given by read_sensor_data) and sends them to the PC
-
- 
  */
 
 
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
-const int analogOutPin = 9; // Analog output pin that the LED is attached to
-const int rolling_average_number = 200;
+const int analogInPin = A0; 
+const int rolling_average_number = 200; //number of iterations for the rolling average
 
 void setup() {
   // initialize serial communications at 9600 bps:
@@ -15,23 +12,19 @@ void setup() {
 }
 
 void loop() {
-  // read the analog in value:
-  int total = 0;
-  int square_total = 0;
+  int average = 0;
+  int square_average = 0;
   for (int i=1;i <= rolling_average_number;i++){
-    int sensor_value = read_sensor_data();
-    total = total * (i/(i+1));
-    square_total = square_total * (i/(i+1));
-    total = total + (1/(i+1))* sensor_value; //run a rolling average
-    square_total = square_total + (sensor_value ^ sensor_value) * (1/i);
     delay(2); // wait 2 milliseconds to let the ADC settle
+    int sensor_value = read_sensor_data();
+    average = (average * i + sensor_value)/(i+1); //run rolling average
+    square_average = (square_average * i + (sensor_value*sensor_value))/(i+1); //run rolling average on the square of sensor value
   }
-  // print the results to the serial monitor:                      
-  int average = total;
-  int square_average = square_total;
-  int variance = abs(square_average - average^2);
+//calculte standard deviation                    
+  int variance = abs(square_average - average^2); // E[x^2] - E[x]^2
   int standard_deviation = sqrt(variance);
   
+// print the results to the serial monitor:  
   Serial.print("\nmean =");
   Serial.print(average);
   Serial.print("\nSD =");
